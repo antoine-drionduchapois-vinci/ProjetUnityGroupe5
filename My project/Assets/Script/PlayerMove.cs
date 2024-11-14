583d44b445c4082dbc518cdd48d50b158d4954aa
antoine-drionduchapois-vinci <128791838+antoine-drionduchapois-vinci@users.noreply.github.com>
2024-11-14 20:53:31 +0100

    Update PlayerMove.cs

--- My project/Assets/Script/PlayerMove.cs
+++ My project/Assets/Script/PlayerMove.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class PlayerMove : MonoBehaviour
 {
     public float moveSpeed = 3;
     // Start is called before the first frame update
