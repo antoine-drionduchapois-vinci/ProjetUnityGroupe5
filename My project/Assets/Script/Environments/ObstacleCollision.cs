45a81ca3d7d719f19ed62407e8db5b235ba46cda
sungrab tenzin <sungrab.tenzin@student.vinci.be>
2024-11-19 17:45:50 +0100

    add coins

--- My project/Assets/Script/Environments/ObstacleCollision.cs
+++ My project/Assets/Script/Environments/ObstacleCollision.cs
@@ -8,15 +8,18 @@ public class ObstacleCollision : MonoBehaviour
     // Start is called before the first frame update
     public GameObject thePlayer;
     public GameObject charModel;
-   void OnTriggerEnter(Collider other){
+    public GameObject LevelControl;
+    void OnTriggerEnter(Collider other){
 
-      
         
         this.gameObject.GetComponent<BoxCollider>().enabled = false;
         thePlayer.GetComponent<PlayerMove>().enabled = false;
         Debug.Log("Collision detected");
         charModel.GetComponent<Animator>().Play("Stumble Backwards");
-        
-        
-   }
+
+        LevelControl.GetComponent<LevelDistance>().enabled = false;
+        LevelControl.GetComponent<EndRunSequence>().enabled = true;
+
+
+    }
 }
