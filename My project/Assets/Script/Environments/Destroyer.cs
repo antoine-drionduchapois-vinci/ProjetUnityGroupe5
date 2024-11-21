80898384b65b18dd037e518d3e021af809b7b81a
Simon Sabot <simon.sabot@gmail.com>
2024-11-21 16:40:05 +0100

    le niveau a plusieur phase , et tout s emboite bien

--- My project/Assets/Script/Environments/Destroyer.cs
+++ My project/Assets/Script/Environments/Destroyer.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 public class Destroyer : MonoBehaviour
 {
     public string parentName;
-   
+    public GameObject player;
 
     void Start()
     {
+       player = GameObject.Find("Player");
         parentName = transform.name;
         StartCoroutine(DestroyClone());
     }
@@ -18,10 +19,11 @@ public class Destroyer : MonoBehaviour
         while (true)
         {
             yield return new WaitForSeconds(40);
-
-            // Check for the correct name or use a more flexible condition as needed
-            if (parentName.Contains("Section(Clone)"))
+           
+            if (player.transform.position.z - gameObject.transform.position.z > 200)
             {
+                if (parentName.Contains("SectionE(Clone)"))
+                    yield return new WaitForSeconds(120);
                 Destroy(gameObject);
                 yield break; // Exit the loop once this object is destroyed
             }
--- My project/Assets/Script/Environments/GenerateLevel.cs
+++ My project/Assets/Script/Environments/GenerateLevel.cs
@@ -1,47 +1,108 @@
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GenerateLevel : MonoBehaviour
 {
-    public GameObject[] section; // Array of section prefabs
+    public GameObject[] sectionOutside; // Array of section prefabs
+    public GameObject[] sectioninside;
     public int zPos = 30; // Z position for the next section
     public bool creatingSection = false;
     public int secNumb;
-
-    public float initialWaitTime = 2f; // Starting generation wait time
-    public float minWaitTime = 0.4f; // Minimum generation wait time
+    public LevelDistance distancePlayed;
+    public float initialWaitTime = 4f; // Starting generation wait time
+    public float minWaitTime = 1f; // Minimum generation wait time
     public float speedIncreaseRate = 0.4f; // Rate at which player speed increases per second
     private float currentWaitTime;
-
+    public GameObject Canion;
+    public int distance;
+    public GameObject Temple;
+    bool intersection = false;
+    GameObject player;
     void Start()
     {
-        // Set the starting wait time
-        currentWaitTime = initialWaitTime;
+        player = GameObject.Find("Player");
+            // Set the starting wait time
+            currentWaitTime = initialWaitTime;
     }
 
     void Update()
     {
+ 
+
         if (!creatingSection)
         {
-            creatingSection = true;
-            StartCoroutine(GenerateSection());
+            if (intersection) { StartCoroutine(waitForCanion()); }
+            else
+            {
+                creatingSection = true;
+                StartCoroutine(GenerateSection());
+            }
         }
 
         // Gradually decrease the wait time based on speedIncreaseRate
         currentWaitTime = Mathf.Max(minWaitTime, currentWaitTime - (speedIncreaseRate * Time.deltaTime));
     }
+    IEnumerator waitForCanion()
+    {
+        distance = distancePlayed.distanceCount;
+        if (distance > 100)
+        {
+            intersection=false;
+        }
+            yield return new WaitForSeconds(currentWaitTime);
+    }
 
     IEnumerator GenerateSection()
     {
-        // Generate a random section
-        secNumb = Random.Range(0, section.Length);
-        Instantiate(section[secNumb], new Vector3(0, 0, zPos), section[secNumb].transform.rotation);
+        Debug.Log(zPos -(int) player.transform.position.z);
+        if ((zPos - (int)player.transform.position.z<1000))
+        {
+            distance = distancePlayed.distanceCount;
+            if (distance > 90)
+            {
+                // Generate a random section
+                secNumb = Random.Range(0, sectioninside.Length);
+                GameObject g = Instantiate(sectioninside[secNumb], new Vector3(35, 0, zPos), sectioninside[secNumb].transform.rotation);
+                g.GetComponent<Destroyer>().enabled = true;
+                // Increment the zPos for the next section
+                zPos += 187;
 
-        // Increment the zPos for the next section
-        zPos += 30;
+                // Wait before generating the next section
+            }
 
-        // Wait before generating the next section
 
+            else if (distance < 30)
+            {
+                // Generate a random section
+                secNumb = Random.Range(0, sectionOutside.Length);
+                GameObject g = Instantiate(sectionOutside[secNumb], new Vector3(0, 0, zPos), sectionOutside[secNumb].transform.rotation);
+                g.GetComponent<Destroyer>().enabled = true;
+                // Increment the zPos for the next section
+                zPos += 30;
+                secNumb = Random.Range(0, sectionOutside.Length);
+                g = Instantiate(sectionOutside[secNumb], new Vector3(0, 0, zPos), sectionOutside[secNumb].transform.rotation);
+                g.GetComponent<Destroyer>().enabled = true;
+                // Increment the zPos for the next section
+                zPos += 30;
+
+                // Wait before generating the next section
+            }
+            else
+            {
+                // instanciate the canion
+                zPos += 200;
+                GameObject g = Instantiate(Canion, new Vector3(+20.70001f, -1, zPos), Quaternion.identity);
+                g.GetComponent<Destroyer>().enabled = true;
+                // Increment the zPos for the next section
+                zPos += 400;
+                g = Instantiate(Temple, new Vector3(0, 0, zPos), Quaternion.identity);
+                g.GetComponent<Destroyer>().enabled = true;
+                zPos += 280;
+                intersection = true;
+                // Wait before generating the next section
+            }
+        }
         yield return new WaitForSeconds(currentWaitTime);
 
         creatingSection = false;
